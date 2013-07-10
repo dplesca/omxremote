96dc5e34ac732425566dbd3ddafd53f66e834380
Dragos Plesca <dragos.plesca@gmail.com>
2013-07-10 21:28:42 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var gh = require('grasshopper'),
+	request = require('request'),
+	omx = require('omxcontrol'),
+	fs = require('fs');
+
+var path = '/media/768E92E48E929BE5/', files = fs.readdirSync(path);
+console.log('test' + files.length);
+
+gh.get('/', function() {
+	this.model['files'] = files;
+	this.render('index');
+});
+gh.serve(8080);
+
+/*omx.start(filename);
+
+omx.pause();
+
+omx.quit();*/
