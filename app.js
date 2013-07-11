0425db4e71a0bb291b01f322327f607eefce471a
Dragos Plesca <plescadragoss@yahoo.com>
2013-07-11 21:10:41 +0300

    Adding hasing for filenames and recursive path parsing

--- app.js
+++ app.js
@@ -3,27 +3,42 @@ var gh = require('grasshopper'),
 	omx = require('omxcontrol'),
 	fs = require('fs');
 
-var path = '/media/768E92E48E929BE5/', files = fs.readdirSync(path);
+var path = '/media/768E92E48E929BE5/';
 console.log('test' + files.length);
 
 gh.get('/', function() {
 	this.model['files'] = [];
-	var nr_files = files.length;
-	for( i = 0; i < nr_files; i++ ){
-		this.model['files'].push({"file" : files[i]});
-	}
+	var self = this;
+	var fs = require('fs');
+	var traverseFileSystem = function (currentPath, self) {
+	
+		var files = fs.readdirSync(currentPath);
+		for (var i in files) {
+			var currentFile = currentPath + '/' + files[i];
+			var stats = fs.statSync(currentFile);
+			if (stats.isFile()) {
+				self.model['files'].push({"file" : files[i], "hash": new Buffer(currentFile).toString('base64')});
+				//console.log(currentFile);
+			}
+			else if (stats.isDirectory()) {
+	 			traverseFileSystem(currentFile);
+			}
+		}
+	};
+	traverseFileSystem(path, self);
+	
 	this.model['files'] = files;
 	this.render('index');
 });
 
 gh.get('/file/{name}', function(args) {
-    this.model['name'] = args.name;
+    this.model['name'] = new Buffer(args.name, 'base64').toString('ascii');
     this.render('file');
     this.renderText('1');
 });
 
 gh.post('/file/{name}/start', function(args) {
-	var filename = path + args.name.replace(/~/g, ".");
+	var filename = path + new Buffer(args.name, 'base64').toString('ascii');
 	omx.start(filename);
 	this.renderText('1');
 });
