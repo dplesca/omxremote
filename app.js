6082588854fbc85b7ea28c0cc1d29c7640a5444c
Dragos Plesca <dragos.plesca@gmail.com>
2013-07-11 21:34:54 +0300

    Some fixes to work properly

--- app.js
+++ app.js
@@ -3,42 +3,43 @@ var gh = require('grasshopper'),
 	omx = require('omxcontrol'),
 	fs = require('fs');
 
-var path = '/media/768E92E48E929BE5/';
-console.log('test' + files.length);
+//var path = '/media/768E92E48E929BE5/';
+var path = '/home/pi/finished/';
 
 gh.get('/', function() {
-	this.model['files'] = [];
-	var self = this;
+	var all_files = [];
 	var fs = require('fs');
-	var traverseFileSystem = function (currentPath, self) {
+	var traverseFileSystem = function (currentPath, all_files) {
 	
 		var files = fs.readdirSync(currentPath);
 		for (var i in files) {
 			var currentFile = currentPath + '/' + files[i];
 			var stats = fs.statSync(currentFile);
-			if (stats.isFile()) {
-				self.model['files'].push({"file" : files[i], "hash": new Buffer(currentFile).toString('base64')});
+			if (stats.isFile() &&  files[i].match(/(.+).(mkv|mp4|avi)$/)) {
+				all_files.push({"file" : files[i], "hash": new Buffer(currentFile).toString('base64')});
 				//console.log(currentFile);
 			}
 			else if (stats.isDirectory()) {
-	 			traverseFileSystem(currentFile);
+	 			traverseFileSystem(currentFile, all_files);
 			}
 		}
+		return all_files;
 	};
-	traverseFileSystem(path, self);
-	
-	this.model['files'] = files;
+	this.model['files'] = traverseFileSystem(path, all_files);
+	//console.log(this.model['files']);
 	this.render('index');
 });
 
 gh.get('/file/{name}', function(args) {
     this.model['name'] = new Buffer(args.name, 'base64').toString('ascii');
+    this.model['hash'] = args.name;
+    console.log(this.model['name']);
     this.render('file');
     this.renderText('1');
 });
 
 gh.post('/file/{name}/start', function(args) {
-	var filename = path + new Buffer(args.name, 'base64').toString('ascii');
+	var filename = new Buffer(args.name, 'base64').toString('ascii');
 	omx.start(filename);
 	this.renderText('1');
 });
