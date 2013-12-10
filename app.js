d9f771c4349dc67eff6d9b36abde88430d789e46
Plesca Dragos <plescadragoss@yahoo.com>
2013-12-10 14:09:58 +0200

    added ua-parser and changed indentation to spaces

--- app.js
+++ app.js
@@ -9,70 +9,71 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	var all_files = [],
-		fs = require('fs');
-		traverseFileSystem = function (currentPath, all_files) {
-	
-		var files = fs.readdirSync(currentPath);
-		for (var i in files) {
-			var currentFile = currentPath + '/' + files[i];
-			var stats = fs.statSync(currentFile);
-			if (stats.isFile() &&  files[i].match(/(.+).(mkv|mp4|avi)$/)) {
-				all_files.push({"file" : files[i], "hash": new Buffer(currentFile).toString('base64')});
-			}
-			else if (stats.isDirectory()) {
-	 			traverseFileSystem(currentFile, all_files);
-			}
-		}
-		return all_files;
-	};
-	var files = traverseFileSystem(path, all_files);
-	files.sort(function (a, b) {
-		return a.file.toLowerCase().localeCompare(b.file.toLowerCase());
-	});
+    var r = require('ua-parser').parse(req.headers['user-agent']),
+        all_files = [],
+        fs = require('fs');
+        traverseFileSystem = function (currentPath, all_files) {
+    
+        var files = fs.readdirSync(currentPath);
+        for (var i in files) {
+            var currentFile = currentPath + '/' + files[i];
+            var stats = fs.statSync(currentFile);
+            if (stats.isFile() &&  files[i].match(/(.+).(mkv|mp4|avi)$/)) {
+                all_files.push({"file" : files[i], "hash": new Buffer(currentFile).toString('base64')});
+            }
+            else if (stats.isDirectory()) {
+                traverseFileSystem(currentFile, all_files);
+            }
+        }
+        return all_files;
+    };
+    var files = traverseFileSystem(path, all_files);
+    files.sort(function (a, b) {
+        return a.file.toLowerCase().localeCompare(b.file.toLowerCase());
+    });
     res.render('home', {"files" : files});
 });
 
 app.get('/file/:name', function(req, res){
-	var params = {};
-	params.name = new Buffer(req.params.name, 'base64').toString('ascii');
+    var params = {};
+    params.name = new Buffer(req.params.name, 'base64').toString('ascii');
     params.hash = req.params.name;
     res.render('file', params);
 });
 
 app.post('/file/:name/start', function(req, res) {
-	var filename = new Buffer(req.params.name, 'base64').toString('ascii');
-	omx.start(filename);
-	res.send(200);
+    var filename = new Buffer(req.params.name, 'base64').toString('ascii');
+    omx.start(filename);
+    res.send(200);
 });
 
 app.post('/file/:name/pause', function(req, res) {
-	omx.pause();
-	res.send(200);
+    omx.pause();
+    res.send(200);
 });
 
 app.post('/file/:name/play', function(req, res) {
-	omx.pause();
-	res.send(200);
+    omx.pause();
+    res.send(200);
 });
 
 app.post('/file/:name/stop', function(req, res) {
-	omx.quit();
-	res.send(200);
+    omx.quit();
+    res.send(200);
 });
 
 /*
 app.post('/file/:name/forward', function(args) {
-	omx.forward();
-	res.send(200);
+    omx.forward();
+    res.send(200);
 });
 app.post('/file/:name/backward', function(args) {
-	omx.backward();
-	res.send(200);
+    omx.backward();
+    res.send(200);
 });
 app.post('/file/:name/subs', function(args) {
-	omx.subs();
-	res.send(200);
+    omx.subs();
+    res.send(200);
 });
 */
 
